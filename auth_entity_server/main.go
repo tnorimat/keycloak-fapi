5a634b3255171e3d1c55d049b3fabbca7e3fdbe9
Takashi Norimatsu <takashi.norimatsu.ws@hitachi.com>
2021-07-12 18:17:36 +0900

    tier 2

--- auth_entity_server/main.go
+++ auth_entity_server/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"encoding/json"
+	"log"
 	"net/http"
 )
 
@@ -13,21 +14,31 @@ type Account struct {
 	Name string `json:"name"`
 }
 
+type AuthDelegateRequest struct {
+	BindingMessage  string `json:"binding_message"`
+	LoginHint       string `json:"login_hint"`
+	ConsentRequired bool   `json:"is_consent_required"`
+	Scope           string `json:"scope"`
+	AcrValues       string `json:"acr_values"`
+}
+
 func accountsHandler(w http.ResponseWriter, r *http.Request) {
+	log.Printf("Incoming request from %s.", r.Host)
 	// TODO : implement authentication entity logic
-	a := Accounts{[]Account{Account{"John"}, Account{"Sary"}}}
 
-	j, err := json.Marshal(a)
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
-	}
+	// receive json and Authorization header for bearer token for token authentication afterwards
+	var authDelegateRequest AuthDelegateRequest
+	json.NewDecoder(r.Body).Decode(&authDelegateRequest)
+	log.Printf("    acr_values : %s.", authDelegateRequest.AcrValues)
+	log.Printf("    binding_message : %s.", authDelegateRequest.BindingMessage)
+	//log.Printf("    is_consent_required : %s.", authDelegateRequest.ConsentRequired)
+	log.Printf("    login_hint : %s.", authDelegateRequest.LoginHint)
+	log.Printf("    scope : %s.", authDelegateRequest.Scope)
 
-	w.Header().Set("Content-Type", "application/json; charset=UTF-8")
-	w.Write(j)
 }
 
 func main() {
+	log.Println("Auth Entity Server booted.")
 	http.HandleFunc("/", accountsHandler)
-	http.ListenAndServe(":3000", nil)
+	http.ListenAndServe(":3001", nil)
 }
